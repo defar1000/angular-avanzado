d937f272382603282a97741013994c8aacb10838
David Rios <david-0214@hotmail.com>
2022-12-15 01:17:19 -0500

    Seccion 3 - 17 - Se crean componentes para auth: login, register - Se crean componentes para pages: dashboard, notpagesfound - Se crean componentes para shared: breadcrumbs, header, sidebar

---
+++ adminpro/src/app/pages/notpagesfoud/notpagesfoud.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notpagesfoud',
+  templateUrl: './notpagesfoud.component.html',
+  styles: [
+  ]
+})
+export class NotpagesfoudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
