4c3f81e886e9275a08ef43f91780fc6267e65c0b
David Rios <david-0214@hotmail.com>
2023-01-09 00:18:48 -0500

    seccion 3 - 36 - correccion a las rutas hijas de auth

--- adminpro/src/app/app-routing.module.ts
+++ adminpro/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthRoutingModule } from './auth/auth.routing';
+import { NotpagesfoudComponent } from './notpagesfoud/notpagesfound.component';
 import { PagesRoutingModule } from './pages/pages.routing';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // '/dashboard' PagesRoutingModule
+  // '/auth' AuthRoutingModule
+  { path: '**', component:NotpagesfoudComponent }
+];
 
 @NgModule({
   declarations: [],
--- adminpro/src/app/auth/auth.routing.ts
+++ adminpro/src/app/auth/auth.routing.ts
@@ -2,13 +2,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { NotpagesfoudComponent } from '../notpagesfoud/notpagesfound.component';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-  
-    { path: '**', component:NotpagesfoudComponent }
+    { path: 'register', component: RegisterComponent },  
 ];
 
 @NgModule({
