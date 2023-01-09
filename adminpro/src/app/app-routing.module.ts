d35c0689e4330b1dbff18a27e88124b8489789e8
David Rios <david-0214@hotmail.com>
2023-01-09 10:35:02 -0500

    secccion 4 - 37 - Se añaden rutas a partir del path dashboard - Fallan rutas a partir del path dashboard

--- adminpro/src/app/app-routing.module.ts
+++ adminpro/src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AuthRoutingModule } from './auth/auth.routing';
-import { NotpagesfoudComponent } from './notpagesfoud/notpagesfound.component';
 import { PagesRoutingModule } from './pages/pages.routing';
+
+import { NotpagesfoudComponent } from './notpagesfoud/notpagesfound.component';
 
 const routes: Routes = [
   // '/dashboard' PagesRoutingModule
   // '/auth' AuthRoutingModule
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component:NotpagesfoudComponent }
 ];
 
 @NgModule({
-  declarations: [],
   imports: [
     RouterModule.forRoot( routes ),
     PagesRoutingModule,
